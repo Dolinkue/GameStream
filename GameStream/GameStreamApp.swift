2b56fcdfeb8656d7d1e4d6b8ce201d94d4a15912
dolinkue <dolinkue_n@me.com>
2022-03-25 15:58:44 -0300

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Nicolas Dolinkue on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
