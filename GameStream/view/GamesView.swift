84f390e251f6d3b9be15126e45d67c3478f6d3c7
dolinkue <dolinkue_n@me.com>
2022-03-30 17:22:28 -0300

    mostrando en gamesview

--- GameStream/view/GamesView.swift
+++ GameStream/view/GamesView.swift
@@ -6,16 +6,119 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct GamesView:View {
     
     //se comunica con el view model para buscar la info
     @ObservedObject var todosLosVideojuegos = ViewModel()
     
+    
+    // variables que capturan los datos del view model
+    @State var gameviewIsActive: Bool = false
+        @State var url:String = ""
+        @State var titulo:String = ""
+        @State var studio:String = ""
+        @State var calificacion: String = ""
+        @State var anoPublicacion: String = ""
+        @State var descripcion:String = ""
+        @State var tags:[String] = [""]
+        @State var imgsUrl: [String] = [""]
+        
+        let formaGrid = [
+        
+            GridItem(.flexible()),
+            GridItem(.flexible())
+        
+        ]
+        
     var body: some View{
         
         
-        Text("Hola desde GamesView").navigationBarHidden(true).navigationBarBackButtonHidden(true)
+        ZStack{
+         
+            Color(red: 19/255, green: 30/255, blue: 53/255, opacity: 1.0).ignoresSafeArea()
+            
+            
+            VStack{
+                Text("Juegos")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(EdgeInsets(top: 16, leading: 0, bottom: 64, trailing: 0))
+                
+                ScrollView{
+                    
+                    // matriz vertical de como se va a mostrar
+                    LazyVGrid(columns:formaGrid,spacing:8){
+                        
+                        ForEach(todosLosVideojuegos.gamesInfo, id: \.self){
+                            
+                            juego in
+                            
+                            Button(action: {
+                                
+                                url = juego.videosUrls.mobile
+                                titulo = juego.title
+                                studio = juego.studio
+                                calificacion = juego.contentRaiting
+                                anoPublicacion = juego.publicationYear
+                                descripcion = juego.description
+                                tags = juego.tags
+                                imgsUrl = juego.galleryImages
+                     
+                                print("Pulse el juego \(titulo)")
+                                
+                               
+                                
+                                
+                            }, label: {
+                                
+                                // se bajo una depencia para que funcione desde una URL
+                                KFImage(URL(string: juego.galleryImages[0])!)
+                                                                    .resizable()
+                                                                    .aspectRatio(contentMode: .fit)
+                                                                    .clipShape(RoundedRectangle.init(cornerRadius: 4))
+                                                                
+                                                                    .padding(.bottom,12)
+                                
+                                
+                            
+                            })
+                            
+                        }
+                        
+                        
+                        
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                }
+                
+                
+                
+                
+                
+                
+            }.padding(.horizontal,6)
+            
+            
+            
+            
+            
+            
+           
+            
+            
+            
+            
+            
+            
+        }.navigationBarHidden(true).navigationBarBackButtonHidden(true)
             .onAppear(perform: {
             
             //Muestra la información del primer elemento del json
@@ -25,7 +128,6 @@ struct GamesView:View {
         
     }
 }
-
 struct GamesView_Previews: PreviewProvider {
     static var previews: some View {
         GamesView()
